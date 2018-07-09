1e483010036de8ce2d27cbb0034db421b4a88878
Diego <diegomallama@gmail.com>
2018-07-09 11:18:12 -0500

    Cambios en vista, templates, llave primaria

--- sitioWeb/blog/views.py
+++ sitioWeb/blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.order_by('fecha_publicado')
     return render(request, 'blog/post_list.html', {'posts': posts})
+    
+
+def detalle_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detalle_post.html', {'post': post}) 
