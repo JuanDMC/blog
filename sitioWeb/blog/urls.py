27b811e552139886e0c522f0ae940a1d64dad15c
Diego <diegomallama@gmail.com>
2018-07-10 11:13:42 -0500

    Agregado, editar post, nuevo post

--- sitioWeb/blog/urls.py
+++ sitioWeb/blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
-    url (r'^post/(?P<pk>[0-9]+)/$', views.detalle_post, name='detalle_post'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.detalle_post, name='detalle_post'),
+    url(r'^post/new/$', views.nuevo_post, name='nuevo_post'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.editar_post, name='editar_post'),
 ]
--- sitioWeb/blog/views.py
+++ sitioWeb/blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -12,3 +13,29 @@ def post_list(request):
 def detalle_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/detalle_post.html', {'post': post}) 
+
+def nuevo_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+                post = form.save(commit=False)
+                post.autor = request.user
+                post.fecha_publicado = timezone.now()
+                post.save()
+                return redirect('detalle_post', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/editar_post.html', {'form': form})
+
+def editar_post(request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.autor = request.user
+                post.save()
+                return redirect('detalle_post', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'blog/editar_post.html', {'form': form})
